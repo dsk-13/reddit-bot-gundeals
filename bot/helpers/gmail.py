24b124d2c39e70533f6e7afb995acd0cb02afc46
Metroshica <landonwo@gmail.com>
2016-08-17 19:22:12 -0400

    Basic email method setup

--- bot/helpers/gmail.py
+++ bot/helpers/gmail.py
@@ -1,4 +1,6 @@
 from helpers import color
+from private import accountinfo
+import smtplib
 
 
 def __init__(self):
@@ -11,111 +13,28 @@ def send_email(self, destination, subject, message):
                             'destination: ' + destination + '\n' +
                             'subject:     ' + subject + '\n' +
                             'message:     ' + message + '\n\n\n')
+    FROM = accountinfo.gmail_user
+    TO = destination
+    SUBJECT = subject
+    TEXT = message
 
-"""Send an email message from the user's account.
-"""
-'''
-import base64
-from email.mime.audio import MIMEAudio
-from email.mime.base import MIMEBase
-from email.mime.image import MIMEImage
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-import mimetypes
-import os
-
-from apiclient import errors
-
-def SendMessage(service, user_id, message):
-  """Send an email message.
-
-  Args:
-    service: Authorized Gmail API service instance.
-    user_id: User's email address. The special value "me"
-    can be used to indicate the authenticated user.
-    message: Message to be sent.
-
-  Returns:
-    Sent Message.
-  """
-  try:
-    message = (service.users().messages().send(userId=user_id, body=message)
-               .execute())
-    print 'Message Id: %s' % message['id']
-    return message
-  except errors.HttpError, error:
-    print 'An error occurred: %s' % error
-
-
-def CreateMessage(sender, to, subject, message_text):
-  """Create a message for an email.
-
-  Args:
-    sender: Email address of the sender.
-    to: Email address of the receiver.
-    subject: The subject of the email message.
-    message_text: The text of the email message.
-
-  Returns:
-    An object containing a base64 encoded email object.
-  """
-  message = MIMEText(message_text)
-  message['to'] = to
-  message['from'] = sender
-  message['subject'] = subject
-  return {'raw': base64.b64encode(message.as_string())}
-
-
-def CreateMessageWithAttachment(sender, to, subject, message_text, file_dir,
-                                filename):
-  """Create a message for an email.
-
-  Args:
-    sender: Email address of the sender.
-    to: Email address of the receiver.
-    subject: The subject of the email message.
-    message_text: The text of the email message.
-    file_dir: The directory containing the file to be attached.
-    filename: The name of the file to be attached.
-
-  Returns:
-    An object containing a base64 encoded email object.
-  """
-  message = MIMEMultipart()
-  message['to'] = to
-  message['from'] = sender
-  message['subject'] = subject
-
-  msg = MIMEText(message_text)
-  message.attach(msg)
-
-  path = os.path.join(file_dir, filename)
-  content_type, encoding = mimetypes.guess_type(path)
-
-  if content_type is None or encoding is not None:
-    content_type = 'application/octet-stream'
-  main_type, sub_type = content_type.split('/', 1)
-  if main_type == 'text':
-    fp = open(path, 'rb')
-    msg = MIMEText(fp.read(), _subtype=sub_type)
-    fp.close()
-  elif main_type == 'image':
-    fp = open(path, 'rb')
-    msg = MIMEImage(fp.read(), _subtype=sub_type)
-    fp.close()
-  elif main_type == 'audio':
-    fp = open(path, 'rb')
-    msg = MIMEAudio(fp.read(), _subtype=sub_type)
-    fp.close()
-  else:
-    fp = open(path, 'rb')
-    msg = MIMEBase(main_type, sub_type)
-    msg.set_payload(fp.read())
-    fp.close()
-
-  msg.add_header('Content-Disposition', 'attachment', filename=filename)
-  message.attach(msg)
-
-  return {'raw': base64.b64encode(message.as_string())}
-
-'''
+    email = """From: %s\nTo: %s\nSubject: %s\n\n%s
+    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)    
+    try:
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.ehlo()
+        server.starttls()
+        server.login(gmail_user, gmail_pwd)
+        server.sendmail(FROM, TO, message)
+        server.close()
+        color.print_color('cyan', '\n\n' +
+                            '----------- EMAIL SENT -----------\n' +
+                            'destination: ' + destination + '\n' +
+                            'subject:     ' + subject + '\n' +
+                            'message:     ' + message + '\n\n\n')
+    except:
+        color.print_color('red', '\n\n' +
+                            '----------- EMAIL FAILED -----------\n' +
+                            'destination: ' + destination + '\n' +
+                            'subject:     ' + subject + '\n' +
+                            'message:     ' + message + '\n\n\n')
