d36eeeb3b889b046b274fc36110ebd71730ea399
Landon Orr <landonwo@gmail.com>
2016-08-28 21:28:05 -0600

    Formatting fixed for e-mail notifications. Now send out an HTML e-mail in MIME format.

--- bot/helpers/gmail.py
+++ bot/helpers/gmail.py
@@ -7,6 +7,8 @@ out how to read the inbox to do that.
 
 from helpers import color, inbox
 from private import accountinfo
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 import smtplib
 
 
@@ -15,40 +17,49 @@ def __init__(self):
 
 
 def send_email(email, username, item, title, permalink, url):
-    FROM = accountinfo.gmail_user
-    TO = email
-    subject = 'We found something'
-    SUBJECT = subject
-    TEXT = inbox.compose_greeting(username) + \
-    "We have found a match for your subscription to '" + item + "'! " + \
-    "Below you will find the details:\n\t \n\t \n" + \
-     "**Deal Title:**\t \n" + \
-     title + "\t \n\t \n" + \
-     "**Links:**\t \n" + \
-     "[Reddit URL](" + permalink + ")" + "     |     " + \
-     "[Sale URL](" + url + ")\n\n" + \
-     "To unsubscribe from these alerts, message /u/GunDealsBot with the subject " + \
-     item + "and with the message body 'Unsubscribe'.\n\n" + \
-     inbox.compose_salutation()
+    subject = 'GunDealsBot found a match for ' + item
+    sender = accountinfo.gmail_user
+    recipient = email
+    htmltext = inbox.compose_greeting(username) + \
+    "<br><br>We have found a match for your subscription to <b>'" + item + "'</b>! " + \
+    "Below you will find the details:" + \
+    "<br><br><b>Deal Title:</b>" + title + \
+    "<br><b>Links:</b>" + \
+    "<br><a href='" + permalink + "'>Reddit Link</a>" +  \
+    "<br><a href='" + url + "'>Sale Link</a>" + \
+    "<br><br>To unsubscribe from these alerts, message /u/GunDealsBot with the subject '" + \
+    item + "' and with the message body 'Unsubscribe'.<br>" + \
+    "--GunDealBot<br><br><code><a href='https://github.com/metroshica/reddit-bot-gundeals'>Source Code on Github<a>" + \
+    "<br><a href='http://reddit.com/u/metroshica'>/u/metroshica</a>" + \
+    "<br><a href='http://reddit.com/r/gundeals'>/r/gundeals</a>"
 
-    message = """From: %s\nTo: %s\nSubject: %s\n\n%s
-    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
+
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = recipient
+
+    plaintext = "Hi there, this is just placeholder text."
+
+    part1 = MIMEText(plaintext, 'plain')
+    part2 = MIMEText(htmltext, 'html')
+
+    msg.attach(part1)
+    msg.attach(part2)
 
     try:
         server = smtplib.SMTP("smtp.gmail.com", 587)
         server.ehlo()
         server.starttls()
         server.login(accountinfo.gmail_user, accountinfo.gmail_pwd)
-        server.sendmail(FROM, TO, TEXT)
+        server.sendmail(sender, recipient, msg.as_string())
         server.close()
         color.print_color('cyan', '\n\n' +
                             '----------- EMAIL SENT -----------\n' +
                             'destination: ' + email + '\n' +
-                            'subject:     ' + subject + '\n' +
-                            'message:     ' + message + '\n\n\n')
+                            'subject:     ' + subject + '\n\n') 
     except:
         color.print_color('red', '\n\n' +
                             '----------- EMAIL FAILED -----------\n' +
                             'destination: ' + email + '\n' +
-                            'subject:     ' + subject + '\n' +
-                            'message:     ' + message + '\n\n\n')
+                            'subject:     ' + subject + '\n\n') 
